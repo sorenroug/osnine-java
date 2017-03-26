8dfd61309e7fbbb4f5fe0e03e75885add095eb8b
Søren Roug <soren.roug@eea.europa.eu>
2017-03-26 15:46:16 +0200

    Made it work.

--- tools/os9tool/src/main/java/CreateModule.java
+++ tools/os9tool/src/main/java/CreateModule.java
@@ -1,23 +1,39 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
+import org.roug.osnine.os9.OS9;
 
 /**
  * This class takes a file and makes it into a memory module.
  * The default module type is Data.
+ * Arguments:
+ * -o output file
+ * -t module type
+ * -n module name
+ * -x Execution offset
+ * -m memory requirement
  */
 public class CreateModule {
 
-    /** List of unrecognized command line arguments. They will be interpreted as names of tables to export. */
+    /** List of unrecognized command line arguments. */
     private String[] extraArguments = new String[0];
 
     /** Path to the file where the result shall be generated into. */
     private String outputFilePath = null;
 
-    private String moduleName = null;
+    private String moduleName = "NoName";
 
-    private String moduleType = null;
+    private int moduleType = 4;
 
+    /** Buffer to build module. Can max be 65536 bytes. */
     private byte[] module = new byte[0x10000];
+
+    private int executionOffset = 0;
+
+    private int memoryRequirement = 0;
+
+    private OutputStream outputStream;
 
     private static void usage(String msg) {
         System.err.println("Error: " + msg);
@@ -25,54 +41,120 @@ public class CreateModule {
         System.exit(2);
     }
 
-    private int read_byte(int location) {
-        return (module[location] & 0xff);
-    }
 
-    private int read_word(int location) {
-        return (module[location] & 0xff) << 8 | (module[location + 1] & 0xff);
-    }
-
-    private int read_triple(int location) {
-        return (module[location] & 0xff) << 16 | (module[location + 1] & 0xff) << 8 | (module[location + 2] & 0xff);
-    }
-
-    private void printHex(String label, int value) {
-        System.out.format("%-20s: %X\n", label, value);
-    }
-
-    private void printString(String label, int location) {
-        // Get length
-        int length = 0;
-        for (int i = location; i < 0x10000; i++) {
-            if (module[i] <= 0) {
-                length = i - location + 1;
-                break;
-            }
-        }
-        module[location + length - 1] &= 0x7f;
-        String buf = new String(module, location, length);
-        module[location + length - 1] |= 0x80;
-        System.out.format("%-20s: %s\n", label, buf);
-    }
-
+    /*
+     * Constructor.
+     */
     public CreateModule(String[] args) throws Exception {
-        OptionParser op = new OptionParser(args, "o:t:n:");
+        long fileSize = 0L;
+        FileInputStream inputStream = null;
+
+        OptionParser op = new OptionParser(args, "f:o:t:n:s:");
 
         outputFilePath = op.getOptionArgument("o");
         if ("-".equals(outputFilePath)) {
             outputFilePath = null; // Linux convention
         }
 
-        moduleType = op.getOptionArgument("t");
-	moduleName = op.getOptionArgument("n");
-
-	extraArguments = op.getUnusedArguments();
-        if (extraArguments.length == 0) {
-            usage("No file argument");
+        String tArg = op.getOptionArgument("t");
+        if (tArg != null) {
+            moduleType = Integer.valueOf(tArg);
         }
-        FileInputStream inputStream = new FileInputStream(extraArguments[0]);
+        String nArg = op.getOptionArgument("n");
+        if (nArg != null) {
+            moduleName = nArg;
+        }
 
+//      extraArguments = op.getUnusedArguments();
+//      if (extraArguments.length == 0) {
+//          usage("No file argument");
+//      }
+        String inputFilePath = op.getOptionArgument("f");
+        if (inputFilePath != null) {
+            inputStream = new FileInputStream(extraArguments[0]);
+            fileSize = inputStream.getChannel().size();
+        }
+
+        String sizeArg = op.getOptionArgument("s");
+        if (sizeArg != null) {
+            fileSize = Long.valueOf(sizeArg);
+        }
+
+        outputStream = System.out;
+        if (outputFilePath != null && !outputFilePath.isEmpty()) {
+            outputStream = new FileOutputStream(outputFilePath);
+        }
+
+        write_word(0, 0x87CD);
+        write_word(2, calculateModuleSize((int) fileSize));
+        write_byte(6, moduleType << 4); // Type / language
+        write_byte(7, 0); // Attributes / revision
+        writeParity();
+        int moduleSize = 9;
+        if (moduleType == 1) {
+            write_word(0x09, executionOffset);
+            write_word(0x0B, memoryRequirement);
+            moduleSize += 4;
+        }
+        write_word(4, moduleSize); // Module name offset
+        writeString(moduleSize, moduleName);
+        moduleSize += moduleName.length();
+        if (inputStream != null) {
+            inputStream.read(module, moduleSize, (int)fileSize);
+            inputStream.close();
+        }
+        moduleSize += fileSize;
+        write_word(2, moduleSize + 3);
+        writeCRC(moduleSize);
+        moduleSize += 3;
+        outputStream.write(module, 0 , moduleSize);
+        outputStream.close();
+    }
+
+    private void write_byte(int location, int value) {
+        module[location] = (byte) (value & 0xff);
+    }
+
+    private void write_word(int location, int value) {
+        byte highByte = (byte) ((value >> 8) & 0xff);
+        module[location] = highByte;
+        byte lowByte = (byte) (value & 0xff);
+        module[location + 1] = lowByte;
+    }
+
+    private void writeString(int location, String name) {
+        byte[] nameBytes = name.getBytes();
+
+        for (int i = 0; i < nameBytes.length; i++) {
+            module[location + i] = nameBytes[i];
+        }
+        module[location + nameBytes.length - 1] |= 0x80;
+    }
+
+    private int calculateModuleSize(int fileSize) {
+        int headerSize = 12;
+        if (moduleType == 1) {
+            headerSize += 4;
+        }
+        headerSize += moduleName.length();
+
+        return fileSize + headerSize;
+    }
+
+    private void writeParity() {
+        byte parity = 0;
+        for (int i = 0; i < 8; i++) {
+            parity = (byte) (parity ^ module[i]);
+        }
+        module[8] = parity;
+    }
+
+    private void writeCRC(int location) {
+        long crc = OS9.CRC24_SEED;
+        long result = OS9.compute_crc(crc, module, location) ^ OS9.CRC24_SEED;
+        module[location + 0] = (byte) ((result >> 16) & 0xFF);
+        module[location + 1] = (byte) ((result >> 8) & 0xFF);
+        module[location + 2] = (byte) (result & 0xFF);
     }
 
 }
