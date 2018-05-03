5119ddc55e2b2e8f7c79855ffc0e9def92f0993b
Søren Roug <soren.roug@eea.europa.eu>
2018-05-03 18:50:07 +0200

    Change to the multiuser config.

--- v6809/src/main/assembler/sysgo_tsmon.asm
+++ v6809/src/main/assembler/sysgo_tsmon.asm
@@ -1,5 +1,5 @@
          nam   SysGo
-         ttl   SysGo that runs TSMON
+         ttl   SysGo that runs Login
 
          ifp1
          use   os9defs
@@ -17,17 +17,20 @@ size     equ   .
 name     equ   *
          fcs   /SysGo/
          fcb   edition
-BootMsg  fcc   "BOOT"
-         fcb   C$CR,C$LF
-         fcb   C$LF
-MsgEnd   equ   *
+*BootMsg  fcc   "BOOT"
+*         fcb   C$CR,C$LF
+*         fcb   C$LF
+*MsgEnd   equ   *
+
 ExecDir  fcc   "Cmds"
          fcb   C$CR
          fcc   ",,,,,," room for patch
-TSMon    fcc   "TSMon"
+
+Login    fcc   "Login"
          fcb   C$CR
          fcc   ",,,,,," room for patch
-TSMonArg fcb   C$CR
+
+LoginArg fcb   C$CR
          fcc   ",,,,,," room for patch
          
 * SysGo entry point
@@ -35,10 +38,10 @@ start    equ   *
          leax  >IcptRtn,pcr     * Set up empty intercept
          os9   F$Icpt   
 
-         leax  >BootMsg,pcr
-         ldy   #MsgEnd-BootMsg
-         lda   #$01
-         os9   I$Write  
+*        leax  >BootMsg,pcr
+*        ldy   #MsgEnd-BootMsg
+*        lda   #$01
+*        os9   I$Write  
 
 * OS9p2 has looked for a module called "Clock" and initialised it.
          ldx   #hwclock
@@ -48,14 +51,14 @@ start    equ   *
          lda   #$04      EXEC.
          os9   I$ChgDir 
 
-FrkTSMon leax  >TSMon,pcr
-         leau  >TSMonArg,pcr
+FrkLogin leax  >Login,pcr
+         leau  >LoginArg,pcr
          ldd   #$0100
-         ldy   #start-TSMonArg   Parameter size
+         ldy   #start-LoginArg   Parameter size
          os9   F$Fork   
          bcs   DeadEnd
          os9   F$Wait   
-         bcc   FrkTSMon
+         bcc   FrkLogin
 DeadEnd  bra   DeadEnd
 
 * Intercept routine
