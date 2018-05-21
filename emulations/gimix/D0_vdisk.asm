6cccd102893498a78e0cd04169f7399bd9ccb8cd
Søren Roug <soren.roug@eea.europa.eu>
2018-05-21 20:49:41 +0200

    /D0 doesn't work.

--- emulations/gimix/D0_vdisk.asm
+++ emulations/gimix/D0_vdisk.asm
@@ -2,7 +2,7 @@
 * disk device descriptor
 *
          nam   D0
-         ttl   40-track floppy disk device descriptor
+         ttl   80-track floppy disk device descriptor
 
          ifp1
          use   /D0/DEFS/os9defs
@@ -12,19 +12,19 @@ atrv     set   ReEnt+rev
 rev      set   $01
          mod   eom,name,tylg,atrv,mgrnam,drvnam
          fcb   $FF mode byte
-         fcb   $FF extended controller address
+         fcb   $0F extended controller address
          fdb   $E040  physical controller address
          fcb   initsize-*-1  initilization table size
          fcb   $01 device type:0=scf,1=rbf,2=pipe,3=scf
          fcb   $00 drive number
-         fcb   $00 step rate
-         fcb   $20 drive device type
-         fcb   $01 media density:0=single,1=double
-         fdb   $0028 number of cylinders (tracks)
-         fcb   $01 number of sides
+         fcb   $83 step rate
+         fcb   $00 drive device type
+         fcb   $03 media density:0=single,1=double
+         fdb   80 number of cylinders (tracks)
+         fcb   $02 number of sides
          fcb   $00 verify disk writes:0=on
-         fdb   $0012 # of sectors per track
-         fdb   $0012 # of sectors per track (track 0)
+         fdb   16    # of sectors per track
+         fdb   10    # of sectors per track (track 0)
          fcb   $02 sector interleave factor
          fcb   $08 minimum size of sector allocation
 initsize equ   *
