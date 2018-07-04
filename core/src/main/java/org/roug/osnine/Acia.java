b4b7d319a523cfeecaeaa2bede3c5cfb5cf4cbd1
Søren Roug <soren.roug@eea.europa.eu>
2018-07-04 18:00:03 +0200

    Javadoc update

--- core/src/main/java/org/roug/osnine/Acia.java
+++ core/src/main/java/org/roug/osnine/Acia.java
@@ -7,12 +7,12 @@ package org.roug.osnine;
 public interface Acia {
 
     /**
-     * Get interrupted by I/O Reader thread and get the byte.
+     * Called when remote end sends a character.
      */
     void dataReceived(int receiveData);
 
     /**
-     * Let the I/O Writer thread wait for the next character.
+     * The Acia waits for the OS to send a character.
      */
     int waitForValueToTransmit() throws InterruptedException;
 
@@ -20,15 +20,13 @@ public interface Acia {
      * Set Data Carrier Detect. In telnet-based emulation this
      * is set to high when a client has connected to the socket.
      * When the client closes the connection, then the DCD goes to false.
-     * In the 6551, the bit is inverted. I.e. bit 5 in the status register
-     * is 1 when there is no carrier.
      *
      * @param detected - if there is a carrier
      */
     void setDCD(boolean detected);
 
     /**
-     * The other end sent EOL. The emulated software might expect CR,
+     * The remote end sent EOL. The emulated software might expect CR,
      * NL or CR+NL.
      */
     void eolReceived();
