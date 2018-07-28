7b7e60b738dc25eda57135faa1f49c3a73a1600f
Søren Roug <soren.roug@eea.europa.eu>
2018-07-28 16:53:47 +0200

    Added the Nordic versions of GO51.

---
+++ os9software/kernel/go51n.asm
@@ -0,0 +1,9 @@
+* Go51 with Danish/Norwegian characters instead of [\]{|}
+      org 1
+ASCII rmb 1
+NORDIC rmb 1
+NORDICKB rmb 1
+
+Charset set NORDIC
+
+ use go51.inc
---
+++ os9software/kernel/go51nkb.asm
@@ -0,0 +1,10 @@
+* Go51 with Danish/Norwegian characters instead of [\]{|}
+* and remapped keyboard
+      org 1
+ASCII rmb 1
+NORDIC rmb 1
+NORDICKB rmb 1
+
+Charset set NORDICKB
+
+ use go51.inc
