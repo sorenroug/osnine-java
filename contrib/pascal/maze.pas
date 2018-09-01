820a7e4716c040277c7d4006ce620a116520253b
Søren Roug <soren.roug@eea.europa.eu>
2018-09-01 09:29:05 +0200

    Typos

--- contrib/pascal/maze.pas
+++ contrib/pascal/maze.pas
@@ -1,6 +1,8 @@
 { Generate a maze.
   The algoritm uses a two-dimensional matrix of cells, where the bottom
-  or the right wall can be absent. }
+  or the right wall can be absent.
+  Pascal does not have a 'continue' statement, therefore the goto 580.
+}
 PROGRAM maze(input);
 label 580,750;
 
@@ -10,6 +12,7 @@ const
 
     NO_FLOOR = 1;
     NO_RIGHT_WALL = 2;
+    VISITED = 4;
 
 type
   direction = (Left, Right, Up, Down);
@@ -19,7 +22,6 @@ var
   rows,cols,c,r,dx,entry,x,cellinx,totcells: integer;
   bottom: boolean;
   d: array[1..10] of direction;
-  visited: array[1..MAXROWS, 1..MAXCOLS] of boolean;
   cells: array[1..MAXROWS, 1..MAXCOLS] of integer;
 
 function random(VAR block:integer) : real; EXTERNAL;
@@ -66,7 +68,7 @@ procedure PrintMaze;
       write('|');
       for c := 1 to cols do
       begin
-        if cells[r, c] < 2 then
+        if (cells[r, c] & NO_RIGHT_WALL) = 0 then
           write('  |')
         else
           write('   ');
@@ -74,7 +76,7 @@ procedure PrintMaze;
       writeln;
       for c := 1 to cols do
       begin
-        if (cells[r, c] mod 2) = 0 then
+        if (cells[r, c] & NO_FLOOR) = 0 then
           write(':--')
         else
           write(':  ');
@@ -97,7 +99,7 @@ procedure FindVisited;
           r := 1;
         c := 1
       end;
-    until visited[r, c] = true;
+    until (cells[r, c] & VISITED) <> 0;
   end;
 
 procedure InitMaze;
@@ -106,7 +108,6 @@ procedure InitMaze;
     for q := 1 to rows do
       for z := 1 to cols do
       begin
-        visited[q,z] := false;
         cells[q,z] := 0;
       end;
   end;
@@ -130,13 +131,13 @@ begin
   r := 1;
   c := entry;
   cellinx := 1;
-  visited[r, c] := true;
+  cells[r, c] := cells[r, c] ! VISITED;
 
   repeat
     dx := 0;
     if c <> 1 then
     begin
-      if visited[r, c - 1] = false then
+      if (cells[r, c - 1] & VISITED) = 0 then
       begin
         AddPossibility(Left)
       end;
@@ -144,7 +145,7 @@ begin
 
     if c <> cols then
     begin
-      if visited[r, c + 1] = false then
+      if (cells[r, c + 1] & VISITED) = 0 then
       begin
         AddPossibility(Right)
       end
@@ -153,7 +154,7 @@ begin
 
     if r > 1 then
     begin
-      if visited[r - 1, c] = false then
+      if (cells[r - 1, c] & VISITED) = 0 then
       begin
         AddPossibility(Up)
       end;
@@ -162,7 +163,7 @@ begin
   750:
     if r < rows then
     begin
-      if visited[r + 1, c] = false then
+      if (cells[r + 1, c] & VISITED) = 0 then
         AddPossibility(Down);
     end
     else
@@ -180,7 +181,7 @@ begin
     case d[x] of
       Down:
         begin
-          cells[r, c] := cells[r, c] + NO_FLOOR;
+          cells[r, c] := cells[r, c] ! NO_FLOOR;
           r := r + 1;
           if r > rows then
           begin
@@ -198,7 +199,7 @@ begin
         end;
       Right:
         begin
-          cells[r, c] := cells[r, c] + NO_RIGHT_WALL;
+          cells[r, c] := cells[r, c] ! NO_RIGHT_WALL;
           c := c + 1
         end;
       Left:
@@ -209,7 +210,7 @@ begin
     end;
 
     cellinx := cellinx + 1;
-    visited[r, c] := true;
+    cells[r, c] := cells[r, c] ! VISITED;
 580:
   until cellinx >= totcells;
 
