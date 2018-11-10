0aee6bfb4029f2ff84fbb5026694625f08dd1ab0
Søren Roug <soren.roug@eea.europa.eu>
2018-11-10 16:41:08 +0100

    Use hashmap for keyboard translator.

--- emulations/mo5/src/main/java/org/roug/osnine/mo5/Keyboard.java
+++ emulations/mo5/src/main/java/org/roug/osnine/mo5/Keyboard.java
@@ -1,8 +1,8 @@
 package org.roug.osnine.mo5;
 
-import java.awt.*;
-import java.awt.event.*;
-
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,7 +19,7 @@ public class Keyboard implements KeyListener {
     private PIA6821MO5 pia;
 
     // translation table from scancode to java keycodes VK_
-    private int[] ftable;
+    private HashMap<Integer, Integer> ftable;
 
     final public int EVENT = 0x8000;
 
@@ -30,83 +30,81 @@ public class Keyboard implements KeyListener {
         this.pia = pia;
         int i;
 
-        this.ftable = new int[PIA6821MO5.KEYS];
+        ftable = new HashMap(100);
 
-        for (i = 0; i < PIA6821MO5.KEYS; i++) {
-            ftable[i] = 0;
-        }
         /* STOP */
         //ftable[0x6E]=0x29;
-  /* 1 .. ACC */
-        ftable[0x5E] = '1';
-        ftable[0x4E] = '2';
-        ftable[0x3E] = '3';
-        ftable[0x2E] = '4';
-        ftable[0x1E] = '5';
-        ftable[0x0E] = '6';
-        ftable[0x0C] = '7';
-        ftable[0x1C] = '8';
-        ftable[0x2C] = '9';
-        ftable[0x3C] = '0';
-        ftable[0x4C] = '-';
-        ftable[0x5C] = '+';
-        ftable[0x6C] = KeyEvent.VK_BACK_SPACE + EVENT;
+        /* 1 .. ACC */
+        ftable.put(Integer.valueOf('1'), 0x5E);
+        ftable.put(Integer.valueOf('2'), 0x4E);
+        ftable.put(Integer.valueOf('3'), 0x3E);
+        ftable.put(Integer.valueOf('4'), 0x2E);
+        ftable.put(Integer.valueOf('5'), 0x1E);
+        ftable.put(Integer.valueOf('6'), 0x0E);
+        ftable.put(Integer.valueOf('7'), 0x0C);
+        ftable.put(Integer.valueOf('8'), 0x1C);
+        ftable.put(Integer.valueOf('9'), 0x2C);
+        ftable.put(Integer.valueOf('0'), 0x3C);
+        ftable.put(Integer.valueOf('-'), 0x4C);
+        ftable.put(Integer.valueOf('+'), 0x5C);
+        //ftable.put(KeyEvent.VK_BACK_SPACE + EVENT, 0x6C);
+        ftable.put(KeyEvent.VK_BACK_SPACE + EVENT, 0x52); // Plain backspace
         /* A .. --> */
-        ftable[0x5A] = 'a';
-        ftable[0x4A] = 'z';
-        ftable[0x3A] = 'e';
-        ftable[0x2A] = 'r';
-        ftable[0x1A] = 't';
-        ftable[0x0A] = 'y';
-        ftable[0x08] = 'u';
-        ftable[0x18] = 'i';
-        ftable[0x28] = 'o';
-        ftable[0x38] = 'p';
-        ftable[0x48] = '/';
-        ftable[0x58] = ')';
+        ftable.put(Integer.valueOf('a'), 0x5A);
+        ftable.put(Integer.valueOf('z'), 0x4A);
+        ftable.put(Integer.valueOf('e'), 0x3A);
+        ftable.put(Integer.valueOf('r'), 0x2A);
+        ftable.put(Integer.valueOf('t'), 0x1A);
+        ftable.put(Integer.valueOf('y'), 0x0A);
+        ftable.put(Integer.valueOf('u'), 0x08);
+        ftable.put(Integer.valueOf('i'), 0x18);
+        ftable.put(Integer.valueOf('o'), 0x28);
+        ftable.put(Integer.valueOf('p'), 0x38);
+        ftable.put(Integer.valueOf('/'), 0x48);
+        ftable.put(Integer.valueOf(')'), 0x58);
         /* Q .. enter */
-        ftable[0x56] = 'q';
-        ftable[0x46] = 's';
-        ftable[0x36] = 'd';
-        ftable[0x26] = 'f';
-        ftable[0x16] = 'g';
-        ftable[0x06] = 'h';
-        ftable[0x04] = 'j';
-        ftable[0x14] = 'k';
-        ftable[0x24] = 'l';
-        ftable[0x34] = 'm';
-        ftable[0x68] = KeyEvent.VK_ENTER + EVENT;
+        ftable.put(Integer.valueOf('q'), 0x56);
+        ftable.put(Integer.valueOf('s'), 0x46);
+        ftable.put(Integer.valueOf('d'), 0x36);
+        ftable.put(Integer.valueOf('f'), 0x26);
+        ftable.put(Integer.valueOf('g'), 0x16);
+        ftable.put(Integer.valueOf('h'), 0x06);
+        ftable.put(Integer.valueOf('j'), 0x04);
+        ftable.put(Integer.valueOf('k'), 0x14);
+        ftable.put(Integer.valueOf('l'), 0x24);
+        ftable.put(Integer.valueOf('m'), 0x34);
+        ftable.put(KeyEvent.VK_ENTER + EVENT, 0x68);
         /* W .. , */
-        ftable[0x60] = 'w';
-        ftable[0x50] = 'x';
-        ftable[0x64] = 'c';
-        ftable[0x54] = 'v';
-        ftable[0x44] = 'b';
-        ftable[0x00] = 'n';
-        ftable[0x10] = ',';
+        ftable.put(Integer.valueOf('w'), 0x60);
+        ftable.put(Integer.valueOf('x'), 0x50);
+        ftable.put(Integer.valueOf('c'), 0x64);
+        ftable.put(Integer.valueOf('v'), 0x54);
+        ftable.put(Integer.valueOf('b'), 0x44);
+        ftable.put(Integer.valueOf('n'), 0x00);
+        ftable.put(Integer.valueOf(','), 0x10);
 
-        ftable[0x20] = '.';
-        ftable[0x30] = '@';
-        ftable[0x6E] = 145 + EVENT;//STOP
-        ftable[0x58] = '*';
+        ftable.put(Integer.valueOf('.'), 0x20);
+        ftable.put(Integer.valueOf('@'), 0x30);
+        ftable.put(145 + EVENT, 0x6E); //STOP
+        ftable.put(Integer.valueOf('*'), 0x58);
 
         /* Specials keys */
-        ftable[0x12] = KeyEvent.VK_INSERT+ EVENT;
-        ftable[0x02] = KeyEvent.VK_DELETE+ EVENT;
-        ftable[0x22] = 36+ EVENT;// Back to top
-        ftable[0x62] = KeyEvent.VK_UP+ EVENT;
-        ftable[0x52] = KeyEvent.VK_LEFT+ EVENT;
-        ftable[0x32] = KeyEvent.VK_RIGHT+ EVENT;
-        ftable[0x42] = KeyEvent.VK_DOWN+ EVENT;
+        ftable.put(KeyEvent.VK_INSERT + EVENT, 0x12);
+        ftable.put(KeyEvent.VK_DELETE + EVENT, 0x02);
+        ftable.put(36 + EVENT, 0x22);// Back to top
+        ftable.put(KeyEvent.VK_UP + EVENT, 0x62);
+        ftable.put(KeyEvent.VK_LEFT + EVENT, 0x52);
+        ftable.put(KeyEvent.VK_RIGHT + EVENT, 0x32);
+        ftable.put(KeyEvent.VK_DOWN + EVENT, 0x42);
         /* space */
-        ftable[0x40] = ' ';
+        ftable.put(Integer.valueOf(' '), 0x40);
         /* SHIFT + BASIC */
-        ftable[0x70] = KeyEvent.VK_F12 + EVENT;//Shift
-        ftable[0x72] = KeyEvent.VK_F11 + EVENT;//Basic
+        ftable.put(KeyEvent.VK_F12 + EVENT, 0x70);//Shift
+        ftable.put(KeyEvent.VK_F11 + EVENT, 0x72);//Basic
 
         /* CNT RAZ */
-        ftable[0x6A] = 17 + EVENT;//CTRL
-        ftable[0x66] = 27 + EVENT;//ESCAPE = raz
+        ftable.put(17 + EVENT, 0x6A);//CTRL
+        ftable.put(27 + EVENT, 0x66);//ESCAPE = raz
     }
 
     public void keyTyped(KeyEvent e) {
@@ -118,7 +116,7 @@ public class Keyboard implements KeyListener {
 
     private void keyTranslator(KeyEvent e, boolean press) {
         int tmpChar = e.getKeyChar();
-        int tmpCode = e.getKeyCode()+ EVENT;
+        int tmpCode = e.getKeyCode() + EVENT;
 
         LOGGER.debug("Key event {}", tmpCode);
 
@@ -191,28 +189,23 @@ public class Keyboard implements KeyListener {
                 keyMemory(0x6A, press);//Ctrl
                 keyMemory(0x64, press);//C
                 return;
-
             default:
                 break;
         }
 
-        for (int i = 0; i < PIA6821MO5.KEYS; i++) {
-            if (ftable[i] >= EVENT) {
-                // Specials keys test
-                if (ftable[i] == tmpCode) { // Match the lookup table
-                    keyMemory(i, press); // Press or release the key
-                    return;
-                }
-            } else {
-                if((tmpChar >= 'A') && (tmpChar <= 'Z')) // Uppercase test
-                    tmpChar = tmpChar + 'a' - 'A'; // Convert to lowercase
-                if (ftable[i] == tmpChar) { // Match the lookup table
-                    keyMemory(i, press); // Press or release the key
-                    return;
-                }
+        if (ftable.get(tmpCode) != null) { // Specials keys test
+            keyMemory(ftable.get(tmpCode), press);
+            return;
+        } else {
+            if((tmpChar >= 'A') && (tmpChar <= 'Z')) // Uppercase test
+                tmpChar = tmpChar + 'a' - 'A'; // Convert to lowercase
+            if (ftable.get(tmpChar) != null) {
+                keyMemory(ftable.get(tmpChar), press);
+                return;
             }
         }
-
+        if (tmpCode != KeyEvent.VK_SHIFT + EVENT && tmpCode != KeyEvent.VK_CONTROL + EVENT)
+            LOGGER.info("Unrecognized Key code: {}", tmpCode);
     }
 
     public void keyPressed(KeyEvent e) {
