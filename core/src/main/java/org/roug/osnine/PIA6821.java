c08c63bd6de4d281fd8f61dae8cd2ee4e4e976d5
Søren Roug <soren.roug@eea.europa.eu>
2018-11-19 22:44:38 +0100

    More PIA tests

--- core/src/main/java/org/roug/osnine/PIA6821.java
+++ core/src/main/java/org/roug/osnine/PIA6821.java
@@ -3,8 +3,16 @@ package org.roug.osnine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+interface Signal {
+    void send(boolean state);
+}
+
+interface OutputPins {
+    void send(int mask, int value, int oldValue);
+}
+
 /**
- * Periphreal interface adapter.
+ * Peripheral interface adapter.
  * There are so many ways this adapter can be wired to devices
  * that it should probably be an abstract class.
  */
@@ -25,24 +33,23 @@ public abstract class PIA6821 extends MemorySegment {
     public static final int BIT6 = 0x40;
     public static final int BIT7 = 0x80;
 
-    /** Control bit for IRQ. */
-    protected static final int CIRQ = 0x01;
-
     /** Bit to select between the OR and DDR. */
     protected static final int SELECT_OR = 0x04;
 
     protected static final int DDRA = 0;
+    protected static final int ORA = 0;
     protected static final int CRA = 1;
     protected static final int DDRB = 2;
+    protected static final int ORB = 2;
     protected static final int CRB = 3;
 
     protected int[] outputRegister = new int[2];
     protected int[] dataDirectionRegister = new int[2];
     protected int[] controlRegister = new int[2];
 
-    /** If PIA has raised IRQ on the bus. */
-    protected boolean[] activeIRQ = new boolean[2];
+    private Signal[] irqOut = new Signal[2];
 
+    private OutputPins[] pinOuts = new OutputPins[2];
 
     /** Reference to CPU for the purpose of sending IRQ. */
     protected Bus8Motorola bus;
@@ -54,6 +61,13 @@ public abstract class PIA6821 extends MemorySegment {
         super(start, start + 4);
         this.bus = bus;
         reset();
+
+        irqOut[A] = (boolean state) -> {};
+        irqOut[B] = (boolean state) -> {};
+
+        pinOuts[A] = (int mask, int value, int oldValue) -> {};
+        pinOuts[B] = (int mask, int value, int oldValue) -> {};
+
     }
 
     private void reset() {
@@ -94,16 +108,7 @@ public abstract class PIA6821 extends MemorySegment {
      * Deactivate IRQ.
      */
     private void deactivateIRQ(int side) {
-        if (activeIRQ[side]) {
-            activeIRQ[side] = false;
-            if (side == B) {
-                bus.signalIRQ(false);
-            }
-            else {
-                LOGGER.info("Lower FIRQ");
-                bus.signalFIRQ(false);
-            }
-        }
+        irqOut[side].send(false);
         controlRegister[side] &= ~(BIT7 | BIT6); // Turn off IRQ bits.
     }
 
@@ -116,8 +121,13 @@ public abstract class PIA6821 extends MemorySegment {
      * Bit 6 and 7 are read only.
      */
     private void writeControlRegister(int side, int val) {
+        if ((val & BIT0) == 0) {
+            disableIRQ(side);
+        } else {
+            enableIRQ(side);
+        }
         controlRegister[side] = (controlRegister[side] & 0xC0) | (val & 0x3F);
-        LOGGER.info("CR Side: {} new value: {}", side, controlRegister[side]);
+        LOGGER.debug("CR Side: {} new value: {}", side, controlRegister[side]);
     }
 
 
@@ -125,57 +135,50 @@ public abstract class PIA6821 extends MemorySegment {
     protected void store(int addr, int operation) {
         switch (addr - getStartAddress()) {
             case DDRA:
-                if (isBitOn(controlRegister[A], SELECT_OR)) {
-                    // Only write bits that are outputs in the data direction register.
-                    outputRegister[A] = (outputRegister[A]
-                            & (dataDirectionRegister[A] ^ 0xFF))
-                            | (operation & dataDirectionRegister[A]);
-                } else {
-                    // Access data direction register
-                    dataDirectionRegister[A] = operation;
-                }
+                setOutputOctet(A, operation);
                 break;
             case CRA:
-                LOGGER.info("CRA Store: {} op: {}", addr, operation);
-                if ((operation & CIRQ) == 0) {
-                    disableIRQ(A);
-                } else {
-                    enableIRQ(A);
-                }
                 writeControlRegister(A, operation);
                 break;
             case DDRB:
-                if (isBitOn(controlRegister[B], SELECT_OR)) {
-                    outputRegister[B] = (outputRegister[B]
-                            & (dataDirectionRegister[B] ^ 0xFF))
-                            | (operation & dataDirectionRegister[B]);
-                } else {
-                    dataDirectionRegister[B] = operation;
-                }
+                setOutputOctet(B, operation);
                 break;
             case CRB:
-                LOGGER.info("CRB Store: {} op: {}", addr, operation);
-                if ((operation & CIRQ) == 0) {
-                    disableIRQ(B);
-                } else {
-                    enableIRQ(B);
-                }
                 writeControlRegister(B, operation);
                 break;
         }
     }
-    /**                 
+
+    /**
      * Send IRQ signal if the IRQ bit is on.
-     */             
+     */
     private void enableIRQ(int side) {
         if (isBitOn(controlRegister[side], BIT7)) {
-            activeIRQ[side] = true;
-        }       
-    }       
-                
+            irqOut[side].send(true);
+        }
+    }
+
     private void disableIRQ(int side) {
-        if (activeIRQ[side]) {
-            activeIRQ[side] = false;
+        irqOut[side].send(false);
+    }
+
+    /**
+     * Set value on a number of output lines simultaneously.
+     * The values on input lines may not be modified.
+     *
+     * @param side - A or B side of the PIA.
+     * @param value - new value to set.
+     */
+    private void setOutputOctet(int side, int value) {
+        if (isBitOn(controlRegister[side], SELECT_OR)) {
+            int outputMask = dataDirectionRegister[side];
+            int oldValue = outputRegister[side] & outputMask;
+
+            outputRegister[side] = (outputRegister[side] & ~outputMask)
+                                 | (outputMask & value);
+            pinOuts[side].send(outputMask, value, oldValue);
+        } else {
+            dataDirectionRegister[side] = value;
         }
     }
 
@@ -191,6 +194,40 @@ public abstract class PIA6821 extends MemorySegment {
             outputRegister[side] |= bitMask;
         else
             outputRegister[side] &= ~bitMask;
+    }
+
+    /**
+     * If bit 0 in CR is 0, then IRQs are disabled,
+     * but the status is kept.
+     */
+    void signalC1(int side) {
+        controlRegister[side] |= BIT7;
+        if (isBitOn(controlRegister[side], BIT0)) {
+            irqOut[side].send(true);
+        }
+    }
+
+    /**
+     * If bit 0 in CR is 0, then IRQs are disabled,
+     * but the status is kept.
+     */
+    void signalC2(int side) {
+        controlRegister[side] |= BIT6;
+        if (isBitOn(controlRegister[side], BIT3)) {
+            irqOut[side].send(true);
+        }
+    }
+
+    /**
+     * Set up a method to be called when something is written to an output
+     * register.
+     */
+    protected void setOutputCallback(int side, OutputPins callback) {
+        pinOuts[side] = callback;
+    }
+
+    protected void setIRQCallback(int side, Signal callback) {
+        irqOut[side] = callback;
     }
 
     private static boolean isBitOn(int x, int n) {
---
+++ core/src/test/java/org/roug/osnine/PIA6821Test.java
@@ -0,0 +1,84 @@
+package org.roug.osnine;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+class PIADummy extends PIA6821 {
+
+    int outputValue = 0;
+
+    boolean signalReceived = false;
+
+    private OutputPins keyboardMatrix = (int mask, int value, int oldValue) -> {
+                outputValue = value & mask;
+            };
+
+    private Signal signalOut = (boolean state) -> { signalReceived = state; };
+
+    PIADummy(int start, Bus8Motorola bus) {
+        super(start, bus);
+        setOutputCallback(PIA6821.B, keyboardMatrix);
+        setIRQCallback(PIA6821.B, signalOut);
+    }
+}
+
+public class PIA6821Test {
+
+    private static final int PIAADDR = 0xF000;
+    private static final int DDRA = PIAADDR + PIA6821.DDRA;
+    private static final int ORA = PIAADDR + PIA6821.ORA;
+    private static final int CRA = PIAADDR + PIA6821.CRA;
+    private static final int DDRB = PIAADDR + PIA6821.DDRB;
+    private static final int ORB = PIAADDR + PIA6821.ORB;
+    private static final int CRB = PIAADDR + PIA6821.CRB;
+
+    /**
+     * Send 1 to an output line - ignored.
+     * Send 1 to an input line - accepted
+     */
+    @Test
+    public void sendToInputLines() {
+        Bus8Motorola bus = new BusStraight();
+        PIADummy pia = new PIADummy(PIAADDR, bus);
+
+        pia.store(CRA, 0);     // Select DDRA
+        pia.store(DDRA, 0xF0); // Make top 4 bits output, the others input
+        pia.store(CRA, 0x04);  // Select output register
+        pia.store(ORA, 0xD0);
+        pia.setInputLine(PIA6821.A, 5, false); // This should be ignored
+        assertEquals(0xD0, pia.load(ORA));
+
+        pia.setInputLine(PIA6821.A, 2, true);
+        assertEquals(0xD4, pia.load(ORA));
+    }
+
+
+    @Test
+    public void sendToOutputLines() {
+        Bus8Motorola bus = new BusStraight();
+        PIADummy pia = new PIADummy(PIAADDR, bus);
+
+        pia.store(CRB, 0);     // Select DDRB
+        pia.store(DDRB, 0xF0); // Make top 4 bits output, the others input
+        pia.store(CRB, 0x04);  // Select output register
+        pia.store(ORB, 0xA3);
+        assertEquals(0xA0, pia.load(ORB));
+        assertEquals(0xA0, pia.outputValue);
+    }
+
+    /**
+     * CB1 is an input line that can then send an IRQ to the CPU.
+     */
+    @Test
+    public void signalCB1() {
+        Bus8Motorola bus = new BusStraight();
+        PIADummy pia = new PIADummy(PIAADDR, bus);
+        assertFalse(pia.signalReceived);
+        pia.store(CRB, 0x05);  // Select output register and IRQ
+        pia.signalC1(PIA6821.B);
+        assertTrue(pia.signalReceived);
+    }
+
+}
