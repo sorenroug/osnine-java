61a7ec08a93a3049b3c2df931834a0e656c116e4
Søren Roug <soren.roug@eea.europa.eu>
2019-03-24 10:36:47 +0100

    More Dragon 64 specific files

---
+++ os9software/dragon64/d0.asm
@@ -0,0 +1,52 @@
+********************************************************************
+* progname - program module
+*
+* Ed.    Comments                                       Who YY/MM/DD
+* ------------------------------------------------------------------
+*  -     Original Dragon Data distribution version
+*
+* Header for : D0
+* Module size: $2E  #46
+* Module CRC : $DC361C (Good)
+* Hdr parity : $CA
+* File Mgr   : RBF
+* Driver     : DDisk
+* Edition    : $52  #82
+* Ty/La At/Rv: $F1 $81
+* Device Descriptor mod, 6809 Obj, re-ent, R/O
+
+         nam   D0
+         ttl   40-track floppy disk device descriptor
+
+         ifp1
+         use   ../DEFS/os9defs
+         endc
+tylg     set   Devic+Objct   
+atrv     set   ReEnt+rev
+rev      set   $01
+         mod   eom,name,tylg,atrv,mgrnam,drvnam
+         fcb   $FF mode byte
+         fcb   $FF extended controller address
+         fdb   $FF40  physical controller address
+         fcb   initsize-*-1  initilization table size
+         fcb   $01 device type:0=scf,1=rbf,2=pipe,3=scf
+         fcb   $00 drive number
+         fcb   $00 step rate
+         fcb   $20 drive device type
+         fcb   $01 media density:0=single,1=double
+         fdb   $0028 number of cylinders (tracks)
+         fcb   $01 number of sides
+         fcb   $00 verify disk writes:0=on
+         fdb   $0012 # of sectors per track
+         fdb   $0012 # of sectors per track (track 0)
+         fcb   $02 sector interleave factor
+         fcb   $08 minimum size of sector allocation
+initsize equ   *
+name     equ   *
+         fcs   /D0/
+mgrnam   equ   *
+         fcs   /RBF/
+drvnam   equ   *
+         fcs   /DDisk/
+         emod
+eom      equ   *
---
+++ os9software/dragon64/sysgo_d64.asm
@@ -0,0 +1,90 @@
+         nam   SysGo
+         ttl   os9 system module    
+
+         ifp1
+         use   defsfile
+         endc
+tylg     set   Systm+Objct   
+atrv     set   ReEnt+rev
+rev      set   $01
+         mod   eom,name,tylg,atrv,start,size
+stack    rmb   200
+size     equ   .
+name     equ   *
+         fcs   /SysGo/
+         fcb   $05 
+BootMsg  fcc   "  OS-9 LEVEL ONE   VERSION 1.2"
+         fcb   C$CR,C$LF
+         fcc   "COPYRIGHT 1980 BY MOTOROLA INC."
+         fcb   C$CR,C$LF
+         fcc   "  AND MICROWARE SYSTEMS CORP."
+         fcb   C$CR,C$LF
+         fcc   "   REPRODUCED UNDER LICENSE"
+         fcb   C$CR,C$LF
+         fcc   "     TO DRAGON DATA LTD."
+         fcb   C$CR,C$LF
+         fcc   "     ALL RIGHTS RESERVED."
+         fcb   C$CR,C$LF
+         fcb   C$LF
+MsgEnd   equ   *
+L00C6    fcc   "Cmds"
+         fcb   C$CR
+         fcc   ",,,,,,,,,,"
+Shell    fcc   "Shell"
+         fcb   C$CR
+         fcc   ",,,,,,,,,,"
+Startup  fcc   "STARTUP -P"
+         fcb   C$CR
+         fcc   ",,,,,,,,,,"
+         
+BasicRst fcb   $55
+         fcb   $00
+         fcb   $74
+         fcb   $12
+         fcb   $7F
+         fcb   $FF
+         fcb   $03
+         fcb   $B7
+         fcb   $FF
+         fcb   $DF
+         fcb   $7E
+         fcb   $F0
+         fcb   $02
+* SysGo entry point
+start    equ   *
+         leax  >IcptRtn,pcr
+         os9   F$Icpt   
+         leax  >BasicRst,pcr
+         ldu   #$0071
+         ldb   #start-BasicRst
+CopyLoop lda   ,x+
+         sta   ,u+
+         decb  
+         bne   CopyLoop
+         leax  >BootMsg,pcr
+         ldy   #MsgEnd-BootMsg
+         lda   #$01
+         os9   I$Write  
+         leax  >L00C6,pcr
+         lda   #$04
+         os9   I$ChgDir 
+         leax  >Shell,pcr
+         leau  >Startup,pcr
+         ldd   #$0100
+         ldy   #$0015
+         os9   F$Fork   
+         bcs   DeadEnd
+         os9   F$Wait   
+FrkShell leax  >Shell,pcr
+         ldd   #$0100
+         ldy   #$0000
+         os9   F$Fork   
+         bcs   DeadEnd
+         os9   F$Wait   
+         bcc   FrkShell
+DeadEnd  bra   DeadEnd
+
+* Intercept routine
+IcptRtn  rti
+         emod
+eom      equ   *
