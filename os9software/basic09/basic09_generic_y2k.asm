5e894e69bd3d56c10959b12d36d8cdfc0d77f94d
Søren Roug <soren.roug@eea.europa.eu>
2019-04-16 12:02:46 +0200

    New algorithm for Y2K that only add 7 more bytes.

--- os9software/basic09/basic09_generic_y2k.asm
+++ os9software/basic09/basic09_generic_y2k.asm
@@ -4,17 +4,7 @@
 * These sequences can cause strange behaviour on a VT100. I have therefore
 * made an edition without these sequences.
 
-* Header for : Basic09
-* Module size: $5A7D  #23165
-* Module CRC : $43E7BE (Good)
-* Hdr parity : $19
-* Exec. off  : $0772  #1906
-* Data size  : $1000  #4096
-* Edition    : $16  #22
-* Ty/La At/Rv: $11 $81
-* Prog mod, 6809 Obj, re-ent, R/O
-
-Y2K equ 0
+Y2K equ 1
 B09EXEC equ 1
 
        org 1
