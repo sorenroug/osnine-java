eeded7c677aa8ecfe0ca86aa0283dda654b7baa6
Søren Roug <soren.roug@eea.europa.eu>
2019-12-08 18:53:25 +0100

    Work on Boot module

--- os9software/gimix/Boot.asm
+++ os9software/gimix/Boot.asm
@@ -1,5 +1,5 @@
  opt l
- nam GIMOX G68 controller boot source
+ nam GIMIX G68 controller boot source
 
  opt -c
 
@@ -8,183 +8,273 @@
  ttl Boot Module
  pag
 
-tylg     set   Systm+Objct   
-atrv     set   ReEnt+rev
-rev      set   $01
-         mod   eom,name,tylg,atrv,start,size
-u0000    rmb   2
+***************************************************************
+*
+* Disk Boot Module Header
+*
+*
+ mod BTEND,BTNAM,SYSTM+OBJCT,REENT+1,BTENT,BTSTA
+BTNAM fcs "Boot"
+ fcb 0 Edition telltale byte
+
+*********************************************************************
+*
+* Static Storage
+*
+*
+ org 0
+V.SEL rmb 2 Drive select reg addr.
 u0002    rmb   2
 u0004    rmb   2
 u0006    rmb   2
 u0008    rmb   2
-u000A    rmb   2
-u000C    rmb   2
-u000E    rmb   1
+V.SECR    rmb   2
+V.DATR    rmb   2
+V.SIDE    rmb   1
 u000F    rmb   4
 u0013    rmb   1
 u0014    rmb   1
 u0015    rmb   1
-u0016    rmb   2
-u0018    rmb   1
-u0019    rmb   1
-size     equ   .
-name     equ   *
-         fcs   /Boot/
-         fcb   $00 
+V.BUF    rmb   2
+V.TRCK    rmb   1
+V.DRV    rmb   1
+BTSTA equ . Total static requirement
+
+
          fcc   "(C)1981Microware"
-L0022    fcb   $00 
-L0023    fcb   $0A 
-L0024    fcb   $10 
-start    equ   *
-         clra  
-         ldb   #$1A
-L0028    pshs  a
-         decb  
-         bne   L0028
-         tfr   s,u
-         ldx   #DPort
-         stx   ,u
+L0022    fcb   $00
+P.T0S    fcb   10  Sectors in Track 0?
+P.SCT    fcb   16  Sectors per track?
+
+*
+* Fd1771 Commands
+*
+
+F.REST equ $0B Restore command
+F.SEEK equ $1B Seek command
+F.STPI equ $4B Step in one track command
+F.READ equ $88 Read sector command
+F.WRIT equ $A8 Write sector command
+F.TYP1 equ $D0 Force type 1 status
+F.WRTR equ $F4 Write track command
+ pag
+****************************************************************
+*
+* Initialize The I/O Port
+*
+*  Input: (U)= Pointer To Global Storage
+*
+*  On Exit: (A) Modified
+*           (X) Modified
+*           (Y) Unchanged
+*           (U) Unchanged
+*
+BTENT clra
+ ldb #BTSTA Get size of needed static
+INILUP pshs a
+ decb
+ bne INILUP
+ tfr s,u Point "u" to static
+ ldx #DPORT Point to controller addr
+ stx V.SEL,U Address of drive select reg.
          leax  $01,x
          stx   u0002,u
          leax  $01,x
          stx   u0004,u
          leax  $02,x
-         lda   #$D0
-         sta   ,x
+ lda #F.Typ1
+ sta 0,x Inz controller chip
          stx   u0006,u
          leax  $01,x
          stx   u0008,u
          leax  $01,x
-         stx   u000A,u
+         stx   V.SECR,u
          leax  $01,x
-         stx   u000C,u
-         lda   #$FF
-         sta   <u0018,u
+         stx   V.DATR,u
+ lda #$FF
+ sta V.TRCK,U Inz to high track count
          lda   [,u]
          anda  #$01
          sta   <u0013,u
-         pshs  u,x,b,a
-         clra  
-         clrb  
+
+*        Fall thru to do the boot
+ pag
+*******************************************************************
+*
+* Bootstrap System From Disk
+*
+*         (U)= Pointer Global Storage
+*
+*  Output: (D)= Length Of Bootstrap Data
+*          (X)= Address Of Start Of Boot Data
+*          (Y) Unchanged
+*          (U) Unchanged
+*
+LBOOT pshs D,X,U Save registers
+ clra
+ clrb
  ldy #1
  ldx D.FMBM
  ldu D.FMBM+2
-         os9   F$SchBit 
-         bcs   BOOTE1
-         exg   a,b
-         ldu   $04,s
-         std   <u0016,u
-         clrb  
-         ldx   #$0000
-         bsr   L00DF
-         bcs   BOOTE1
-         ldd   <$18,y
-         std   ,s
-         os9   F$SRqMem 
-         bcs   BOOTE1
-         stu   $02,s
-         ldu   $04,s
-         ldx   $02,s
-         stx   <u0016,u
-         ldx   <$16,y
-         ldd   <$18,y
-         beq   L00AA
-L0097    pshs  x,b,a
-         clrb  
-         bsr   L00DF
-         bcs   L00AF
-         puls  x,b,a
-         inc   <u0016,u
-         leax  $01,x
-         subd  #$0100
-         bhi   L0097
-L00AA    clra  
-         puls  b,a
-         bra   L00B3
+ OS9 F$SchBit Search for buffer spot
+ bcs BOOTE1 Problem no mem
+ exg a,b Convert page no. to addr
+ ldu 4,s Restore "u"
+ std V.BUF,u Save buffer addr for use
+ clrb
+ ldx #0 Get sector zero
+ bsr READSK
+ bcs BOOTE1 Branch if can't
+ ldd DD.BSZ,Y Get bootstrap size
+ std 0,S Return to caller
+ OS9 F$SRqMem Get boot memory
+ bcs BOOTE1 Branch if none
+ stu 2,S Return to caller
+ ldu 4,S Get static storage
+ ldx 2,s Get mem addr
+ stx V.BUF,u
+ ldx DD.BT+1,Y Get boot sector
+ ldd DD.BSZ,Y Get boot size
+ beq BOOT2
+BOOT1 pshs D,X Save size, sector number
+ clrb
+ bsr READSK Read sector
+ bcs BOOTER
+ puls D,X Retrieve size, sector number
+ inc V.BUF,U Move buffer ptr
+ leax 1,X Up sector number
+ subd #$100 Subtract page read
+ bhi BOOT1 Branch if more
+BOOT2 clra CLEAR Carry
+ puls d
+ bra Bootz
 
-L00AF    leas  $04,s
-BOOTE1    leas  $02,s
-L00B3    puls  u,x
-         leas  <$1A,s
-         rts   
-L00B9    lda   #$01
-         sta   <u0019,u
-         clr   <u0018,u
-         lda   #$05
-L00C3    ldb   #$4B
-         pshs  a
+BOOTER leas 4,S Return scratch
+BOOTE1 leas 2,S Return scratch
+Bootz puls x,u
+ leas BTSTA,s De-allocate static fm stack
+ rts
+ pag
+*********************************************************
+*
+* Restore Drive To Track Zero
+*
+*  Input: (Y)= Pointer To Path Descriptor
+*         (U)= Pointer To Global Storage
+*
+*  If Error: (B)= Error Code & Carry Is Set
+*
+* Note:  We Are Stepping In Several Tracks Before
+*        Issuing The Restore.  As Suggested In The
+*        Application Notes.
+*
+RESTOR    lda   #1
+ sta V.DRV,u
+ clr V.TRCK,U Old track = 0
+ lda #5 Repeat five times
+RESTR2 ldb #F.STPI Step in command
+ pshs A
          eorb  >L0022,pcr
          clr   <u0014,u
-         lbsr  L01BF
-         puls  a
-         deca  
-         bne   L00C3
-         ldb   #$0B
+ lbsr WCR0 Issue command, delay & wait for done.
+ puls A
+ deca DONE Stepping?
+ bne RESTR2 ...no; step again.
+ ldb #F.REST Restore command
          eorb  >L0022,pcr
-         lbra  L01BF
-L00DF    lda   #$91
-         cmpx  #$0000
-         bne   L00F8
-         bsr   L00F8
-         bcs   L00EF
-         ldy   <u0016,u
-         clrb  
-L00EF    rts   
-L00F0    bcc   L00F8
+ lbra WCR0
+ pag
+*************************************************************
+*
+* Read Sector Command
+*
+* Input: B = Msb Of Logical Sector Number
+*        X = Lsb'S Of Logical Sector Number
+*        Y = Ptr To Path Descriptor
+*        U = Ptr To Global Storage
+*
+* Output: 256 Bytes Of Data Returned In Buffer
+*
+* Error: Cc=Set, B=Error Code
+*
+READSK lda #$91 Error retry code
+ cmpx #0 Is this sector zero?
+         bne   RDSK3
+         bsr   RDSK3
+         bcs   Retrn1
+ ldy V.BUF,u Point "y" to dd. info
+ clrb
+Retrn1 rts
+
+
+RDSK1    bcc   RDSK3
          pshs  x,b,a
-         bsr   L00B9
+         bsr   RESTOR
          puls  x,b,a
-L00F8    pshs  x,b,a
-         bsr   L0103
+RDSK3    pshs  x,b,a
+         bsr   READSC
          puls  x,b,a
-         bcc   L00EF
-         lsra  
-         bne   L00F0
-L0103    bsr   L011B
-         bcs   L00EF
-         ldx   <u0016,u
+         bcc   Retrn1
+         lsra
+         bne   RDSK1
+READSC    bsr   PHYSIC
+         bcs   Retrn1
+         ldx   <V.BUF,u
          lda   #$10
          sta   <u0014,u
-         ldb   #$88
-         lbsr  L01BF
+ ldb #F.READ Read sector command
+         lbsr  WCR0
          lbra  L01FE
-L0117    comb  
-         ldb   #$F1
-         rts   
-L011B    lda   #$01
-         sta   <u0019,u
+ pag
+**************************************************************
+*
+* Convert Logical Sector Number To Physical Track And Sector
+*
+*  Input:  B = Msb Of Logical Sector Number
+*          X = Lsb'S Of Logical Sector Number
+*  Output: A = Physical Track Number
+*          Sector Reg = Physical Sector Number
+*  Error:  B = Error Code, W/Carry Set
+*
+
+PHYERR comb
+ ldb #E$SECT Error: bad sector number
+ rts
+
+PHYSIC    lda   #$01
+         sta   <V.DRV,u
          lda   #$20
          sta   u000F,u
-         clr   u000E,u
-         tstb  
-         bne   L0117
-         tfr   x,d
-         cmpd  #$0000
-         beq   L018B
+         clr   V.SIDE,u
+         tstb
+         bne   PHYERR
+ tfr X,D Logical sector (os-9)
+ cmpd #0 Logical sector zero?
+         beq   PHYSC7
          cmpd  $01,y
-         bcc   L0117
+         bcc   PHYERR
          tst   <u0013,u
          bne   L014A
-         subb  >L0023,pcr
+         subb  >P.T0S,pcr
          sbca  #$00
          bcc   L0159
-         clra  
-         addb  >L0023,pcr
-         bra   L018B
-L014A    subb  >L0024,pcr
+         clra
+         addb  >P.T0S,pcr
+         bra   PHYSC7
+L014A    subb  >P.SCT,pcr
          sbca  #$00
          bcc   L0159
-         clra  
-         addb  >L0024,pcr
-         bra   L018B
+         clra
+         addb  >P.SCT,pcr
+         bra   PHYSC7
 L0159    stb   <u0014,u
-         clrb  
+         clrb
          pshs  b
          ldb   <$10,y
-         lsrb  
+         lsrb
          ldb   <u0014,u
          bcc   L0176
-L0168    com   u000E,u
+L0168    com   V.SIDE,u
          bne   L016E
          inc   ,s
 L016E    subb  $03,y
@@ -201,35 +291,35 @@ L017E    lda   <$10,y
          clr   u000F,u
 L0187    puls  a
          addb  $03,y
-L018B    stb   [<u000A,u]
+PHYSC7    stb   [<V.SECR,u]
          ldb   u000F,u
-         orb   <u0019,u
-         stb   <u0019,u
-         ldb   <u0018,u
+         orb   <V.DRV,u
+         stb   <V.DRV,u
+         ldb   <V.TRCK,u
          stb   [<u0008,u]
 L019C    ldb   [,u]
          bitb  #$20
          bne   L019C
-         cmpa  <u0018,u
+         cmpa  <V.TRCK,u
          beq   L01BD
-         sta   <u0018,u
-         sta   [<u000C,u]
+         sta   <V.TRCK,u
+         sta   [<V.DATR,u]
          ldb   #$1B
          eorb  >L0022,pcr
          clr   <u0014,u
-         bsr   L01BF
+         bsr   WCR0
          lda   #$04
          sta   <u0015,u
-L01BD    clrb  
-         rts   
-L01BF    stx   [<u0004,u]
-         lda   <u0019,u
+L01BD    clrb
+         rts
+WCR0    stx   [<u0004,u]
+         lda   <V.DRV,u
          tst   <u0013,u
          beq   L01CC
          ora   #$C0
 L01CC    sta   [,u]
          lda   <u0014,u
-         tst   u000E,u
+         tst   V.SIDE,u
          beq   L01D7
          ora   #$40
 L01D7    ora   #$80
@@ -238,7 +328,7 @@ L01D7    ora   #$80
          beq   L01ED
          orb   <u0015,u
          clr   <u0015,u
-         tst   u000E,u
+         tst   V.SIDE,u
          beq   L01ED
          orb   #$02
 L01ED    stb   [<u0006,u]
@@ -246,7 +336,7 @@ L01F0    lda   [,u]
          bita  #$40
          beq   L01F0
          lda   [<u0006,u]
-         rts   
+         rts
          bita  #$40
          bne   ERRWP
 L01FE    bita  #$04
@@ -257,35 +347,35 @@ L01FE    bita  #$04
          bne   ERSEEK
          bita  #$80
          bne   ERNRDY
-         clrb  
-         rts   
+         clrb
+         rts
 
-ERRCRC    comb  
+ERRCRC    comb
          ldb   #E$CRC
-         rts   
+         rts
 
-ERSEEK comb  
+ERSEEK comb
  ldb   #E$Seek
- rts   
+ rts
 
 L0218    ldb   <u0014,u
          bitb  #$20
          bne   RDERR
-         comb  
+         comb
          ldb   #E$Write
-         rts   
+         rts
 
-RDERR comb  
+RDERR comb
  ldb   #E$Read
- rts   
+ rts
 
-ERNRDY comb  
+ERNRDY comb
  ldb   #E$NotRdy
- rts   
+ rts
 
-ERRWP comb  
+ERRWP comb
  ldb   #E$WP
- rts   
+ rts
 
          emod
-eom      equ   *
+BTEND      equ   *
