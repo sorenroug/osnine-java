c2f454c1c9ff036c65bd39b42a842e99de32e831
Søren Roug <soren.roug@eea.europa.eu>
2020-08-12 12:50:41 +0200

    Init.asm finished

--- os9software/dragon128/Init.asm
+++ os9software/dragon128/Init.asm
@@ -1,30 +1,46 @@
-*****
-*
-* Configuration Module
-*
  nam Init
- ttl Configuration Module
+ ttl OS-9 Configuration Module
+
+************************************************************
+*
+*     Configuration Module
+*
 
  use defsfile
 
- mod INITEND,INITNAM,SYSTM,REENT+1
- fcb $0B    Top of free RAM high byte
- fdb $FFFF     Top of free RAM
- fcb 14        IRQ polling slots
- fcb 14        System device slots
+ mod ConEnd,ConNam,systm,reent+1
 
- fdb SYSGO     Initial module name
- fdb SYSDEV    system device (sysdev)
- fdb SYSTERM
- fdb BOOTST    bootstrap module (bootst)
- fdb ERRMSG
+*
+* configuration constants
+*
+TopRAM1 equ $0B Top of RAM MSB
+TopRAM2 equ $FF Top of RAM middle byte
+TopRAM3 equ $FF Top of RAM LSB
+SizPoll equ 14 irq polling table size
+SizDevic equ 14 device table size
 
-INITNAM  fcs   "Init"
-SYSGO    fcs   "SysGo"
-SYSDEV   fcs   "/D0"
-SYSTERM  fcs   "/term"
-BOOTST   fcs   "Boot"
-ERRMSG   fcs   "errmsg"
+*
+* configuration module body
+*
+ fcb TopRam1,TopRam2,TopRam2
+ fcb SizPoll irq polling table size
+ fcb SizDevic device table size
+ fdb RunMod first executable module
+ fdb DefDir default directory
+ fdb StdIO standard path
+ fdb BootMod bootstrap module
+ ifne EXTERR
+ fdb ErrNam error messages path name
+ endc
 
-         emod
-INITEND  equ   *
+ConNam fcs "Init"
+RunMod fcs "SysGo"
+DefDir fcs "/D0"
+StdIO  fcs "/term"
+BootMod fcs "Boot"
+ ifne EXTERR
+ErrNam fcs "errmsg"
+ endc
+ emod end of configuration module
+
+ConEnd  equ   *
