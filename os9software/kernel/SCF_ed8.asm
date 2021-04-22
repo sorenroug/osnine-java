842a1aefb143ad4a4b62eb812f37d4344eac42f8
Søren Roug <soren.roug@eea.europa.eu>
2021-04-22 21:42:07 +0200

    Changed labels

--- os9software/kernel/SCF_ed8.asm
+++ os9software/kernel/SCF_ed8.asm
@@ -1,5 +1,5 @@
 *
-* Original edition from Dragon Data OS-9
+* Original edition from Dragon Data OS-9 (Level I)
 *
  nam   SCF
 
@@ -22,51 +22,59 @@
 tylg     set   FlMgr+Objct
 atrv     set   ReEnt+rev
 rev      set   $01
-         mod   SCFEnd,SCFNam,tylg,atrv,start,0
+         mod   SCFEnd,SCFNam,tylg,atrv,SCF,0
 
 SCFNam     equ   *
          fcs   /SCF/
          fcb   8
 
-start    equ   *
-         lbra  Create
-         lbra  Open
-         lbra  MakDir
-         lbra  ChgDir
+SCF    equ   *
+         lbra  SCCrea
+         lbra  SCOpen
+         lbra  SCOErr
+         lbra  SCOErr
          lbra  Delete
          lbra  Seek
-         lbra  Read
-         lbra  Write
-         lbra  ReadLn
-         lbra  WriteLn
-         lbra  GetStat
-         lbra  PutStat
-         lbra  Term
+         lbra  SCRead
+         lbra  SCWrite
+         lbra  SCRdLine
+         lbra  SCWrLine
+         lbra  SCGStat
+         lbra  SCPstat
+         lbra  SCClose
 
-* Open/Create entry
-Open
-Create   ldx   PD.DEV,y
-         stx   <$3B,y
-         ldu   PD.RGS,y
-         ldx   R$X,u
+ ttl Open/Close/GetStat etc.
+ page
+***************
+* SCCrea & SCOpen
+*   Process Create/Open Request
+
+* Passed: (Y) = Path Descriptor ptr
+
+SCCrea equ *
+SCOpen ldx PD.DEV,y copy Device Table Ptr
+ stx PD.TBL,y for user GetSts availability
+ ldu PD.RGS,y get caller's register stack
+ ldx R$X,u get pathname ptr
          pshs  y
          os9   F$PrsNam
-         bcs   L00C7
+         bcs   SCOER1
          lda   -1,y
-         bmi   L0053
+         bmi   Open0
          leax  ,y
          os9   F$PrsNam
-         bcc   L00C7
-L0053    sty   R$X,u
+         bcc   SCOER1
+Open0    sty   R$X,u
          puls  y
          lda   #$01
          bita  $01,y
          beq   L009C
          ldd   #$0001
-         os9   F$SRqMem
-         bcs   L00CC
-         stu   PD.BUF,y
-         clrb
+ os9 F$SRqMem allocate buffers
+ bcs OPNER9 ..error; return it
+ stu PD.BUF,y save buffer ptr
+ clrb  
+
          bsr   L008B
 
 * cute message:
@@ -88,7 +96,7 @@ L0097    sta   ,u+
          bne   L0097
 
 L009C    ldu   PD.DEV,y     Get device table entry address
-         beq   MakDir
+         beq   SCOErr
          ldx   $02,u
          lda   PD.PAG,y  Get lines per page
          sta   V.LINE,x
@@ -105,47 +113,45 @@ L009C    ldu   PD.DEV,y     Get device table entry address
          lsla
          ora   ,s+
          os9   I$Attach
-         bcs   L00CC
+         bcs   OPNER9
          stu   $0A,y
 * seek/delete routine
 Seek
 Delete   clra
          rts
-L00C7    puls  pc,y
+SCOER1    puls  pc,y
 
-* ChgDir/MakDir entry
-ChgDir
-MakDir   comb
+SCOErr   comb
          ldb   #E$BPNam
-L00CC    rts
+OPNER9    rts
 
-Term     tst   PD.CNT,y
-         bne   L00E3
+SCClose tst PD.CNT,y Last Image?
+         bne   Close.D
          ldu   PD.FST,y
-         beq   L00D8
+         beq   Close.B
          os9   I$Detach
-L00D8    ldu   PD.BUF,y
-         beq   L00E2
+Close.B    ldu   PD.BUF,y
+         beq   Close.C
          ldd   #$0001
          os9   F$SRtMem
-L00E2    clra
-L00E3    rts
+Close.C    clra
+Close.D    rts
 
-GetStat  ldx   PD.RGS,y
+SCGStat  ldx   PD.RGS,y
          lda   R$B,x
          cmpa  #$00
-         beq   L011C
+         beq   SCPRTN
          pshs  a
          ldd   #D$GSTA
 L00F1    ldx   $03,y
-         ldu   $02,x
-         ldx   ,x
-         addd  $09,x
-         leax  d,x
-         puls  a
-         jmp   ,x                      jump into driver
+ ldu V$STAT,x static storage for driver
+ ldx V$DRIV,x
+ addd M$Exec,x driver's lbra table
+ leax D,X
+ puls a restore status function code
+ jmp 0,x into the hands of a friendly driver
 
-PutStat  ldx   PD.RGS,y
+SCPstat  ldx   PD.RGS,y
          lda   R$B,x
          cmpa  #$00
          beq   L010E
@@ -159,63 +165,78 @@ L0115    lda   ,x+
          sta   ,y+
          decb
          bne   L0115
-L011C    clrb
+SCPRTN    clrb
          rts
 
          comb
-         ldb   #$D0
-L0121    rts
+         ldb   #E$UnkSvc
+SCRETN    rts
 
-Read     lbsr  L0327
-         bcs   L0121
+***************
+* SCRead
+*   Process Read Request
+*
+* Passed: (Y)=File Descriptor Static Storage
+
+SCRead     lbsr  SCALOC
+         bcs   SCRETN
          inc   $0C,y
          ldx   $06,u
-         beq   L016A
+         beq   SCRead3
          pshs  x
          ldx   #$0000
          ldu   $04,u
-         lbsr  L027E
-         bcs   L0143
+         lbsr  GetChr
+         bcs   SCERR
          tsta
-         beq   L0159
+         beq   SCRead2
          cmpa  PD.EOF,y
          bne   L0151
-L0141    ldb   #E$EOF
-L0143    leas  $02,s
+SCEOFX    ldb   #E$EOF
+SCERR    leas  $02,s
          pshs  b
-         bsr   L016A
+         bsr   SCRead3
          comb
          puls  pc,b
-L014C    lbsr  L027E
-         bcs   L0143
+L014C    lbsr  GetChr
+         bcs   SCERR
 L0151    tst   PD.EKO,y
-         beq   L0159
-         lbsr  L0396
-L0159    leax  $01,x
+         beq   SCRead2
+         lbsr  PutDv2
+SCRead2    leax  $01,x
          sta   ,u+
-         beq   L0164
+         beq   SCRead22
          cmpa  <$2B,y
-         beq   L0168
-L0164    cmpx  ,s
+         beq   SCRead25
+SCRead22    cmpx  ,s
          bcs   L014C
-L0168    leas  $02,s
-L016A    ldu   $06,y
+SCRead25    leas  $02,s
+SCRead3    ldu   $06,y
          stx   $06,u
-         lbra  L02D1
+         lbra  IODONE
 
-ReadLn   lbsr  L0327
-         bcs   L0121
+ page
+***************
+* SCRdLine
+*   Buffer a line from the Input device
+*   using line-editing functions
+
+* Passed: (Y)=File Descriptor address
+*         (U)=Caller's register stack
+
+SCRdLine   lbsr  SCALOC
+         bcs   SCRETN
          ldx   $06,u
-         beq   L016A
+         beq   SCRead3
          tst   $06,u
-         beq   L0181
+         beq   SCRd10
          ldx   #$0100
-L0181    pshs  x
+SCRd10    pshs  x
          ldd   #$FFFF
          std   $0D,y
          lbsr  L0231
-L018B    lbsr  L027E
-         lbcs  L0217
+SCRd20    lbsr  GetChr
+         lbcs  SCABT
          tsta
          beq   L01A0
          ldb   #$29
@@ -229,43 +250,43 @@ L01A0    cmpx  $0D,y
          stx   $0D,y
 L01A6    leax  $01,x
          cmpx  ,s
-         bcs   L01B6
+         bcs   SCRd40
          lda   <$33,y
          lbsr  L024F
          leax  -$01,x
-         bra   L018B
-L01B6    lbsr  L029F
+         bra   SCRd20
+SCRd40    lbsr  UPCASE
          sta   ,u+
          cmpa  #$0A
          bra   L01C4
-         lbsr  L0251
+         lbsr  EKOCR
          lda   #$0A
-L01C4    lbsr  L02AF
-         bra   L018B
+L01C4    lbsr  CHKEKO
+         bra   SCRd20
 L01C9    pshs  pc,x
-         leax  >L01DC,pcr
+         leax  >CTLTBL,pcr
          subb  #$29
          lslb
          leax  b,x
          stx   $02,s
          puls  x
          jsr   [,s++]
-         bra   L018B
+         bra   SCRd20
 
-L01DC    bra   L0237        Process PD.BSP
-         bra   L0226        Process PD.DEL
-         bra   L01EE        Process PD.EOR
-         bra   L020D        Process PD.EOF
-         bra   L0255        Process PD.RPR
-         bra   L025E
+CTLTBL    bra   SCBSP        Process PD.BSP
+         bra   SCDEL        Process PD.DEL
+         bra   SCEOL        Process PD.EOR
+         bra   SCEOF        Process PD.EOF
+         bra   SCPRNT        Process PD.RPR
+         bra   SCRPET
          puls  pc
-         bra   L0226
-         bra   L0226
+         bra   SCDEL
+         bra   SCDEL
 
 * Process PD.EOR character
-L01EE    leas  $02,s
+SCEOL    leas  $02,s
          sta   ,u
-         lbsr  L02AF
+         lbsr  CHKEKO
          ldu   $06,y
          leax  $01,x
          stx   $06,u
@@ -276,34 +297,34 @@ L01FF    lda   ,u+
          cmpa  <$2B,y
          bne   L01FF
          leas  $02,s
-         lbra  L02D1
+         lbra  IODONE
 
 * Process PD.EOF
-L020D    leas  $02,s
+SCEOF    leas  $02,s
          leax  ,x
-         lbeq  L0141
+         lbeq  SCEOFX
          bra   L01A0
 
-L0217    pshs  b
+SCABT    pshs  b
          lda   #$0D
          sta   ,u
-         bsr   L0251
+         bsr   EKOCR
          puls  b
-         lbra  L0143
-L0224    bsr   L0237
+         lbra  SCERR
+L0224    bsr   SCBSP
 
 * Process interrupt
-L0226    leax  ,x
+SCDEL    leax  ,x
          beq   L0231
          tst   PD.DLO,y
          beq   L0224
-         bsr   L0251
+         bsr   EKOCR
 L0231    ldx   #$0000
          ldu   PD.BUF,y
          rts
 
 * Process PD.BSP 
-L0237    leax  ,x
+SCBSP    leax  ,x
          lbeq  L02BD
          leau  -1,u
          leax  -1,x
@@ -313,17 +334,18 @@ L0237    leax  ,x
          lda   #$20
          bsr   L024F
 L024C    lda   PD.BSE,y
-L024F    bra   L02C6
+L024F    bra   EKOBYT
 
-L0251    lda   #$0D
-         bra   L02C6
+EKOCR    lda   #$0D
+         bra   EKOBYT
 
 * Process PD.RPR
-L0255    lda   PD.EOR,y
+SCPRNT    lda   PD.EOR,y
          sta   ,u
          bsr   L0231
-L025C    bsr   L02BE
-L025E    cmpx  $0D,y
+L025C    bsr   EKOCHR
+
+SCRPET    cmpx  $0D,y
          beq   L0275
          leax  $01,x
          cmpx  $02,s
@@ -336,16 +358,24 @@ L025E    cmpx  $0D,y
 L0273    leax  -$01,x
 L0275    rts
 
-L0276    pshs  u,y,x
+***************
+* GetChr
+*   get one character from Input device
+
+* Passed:  (Y)=File Descriptor address
+* returns: (A)=char
+*          (B)=error, cc set if error
+
+GetDv2    pshs  u,y,x
          ldx   PD.FST,y
          ldu   PD.DEV,y
-         bra   L0286
+         bra   GetChr1
 
-L027E    pshs  u,y,x
+GetChr    pshs  u,y,x
          ldx   $03,y
          ldu   $0A,y
          beq   L028D
-L0286    ldu   $02,u
+GetChr1    ldu   $02,u
          ldb   <$28,y
          stb   $07,u
 L028D    leax  ,x
@@ -354,11 +384,19 @@ L028D    leax  ,x
          ldu   $02,x
          std   $09,u
          ldu   #$0003
-         lbsr  L03FF
+         lbsr  IOEXEC
 L029D    puls  pc,u,y,x
 
-* Check for forced uppercase
-L029F    tst   PD.UPC,y
+***************
+* UPCASE
+*   Convert lower-case to upper-case
+
+* Passed: (A)=char
+*         (Y)=PD
+* Returns: (A)=upcased
+* Destroys: none
+
+UPCASE    tst   PD.UPC,y
          beq   L02AE
          cmpa  #$61
          bcs   L02AE
@@ -367,26 +405,27 @@ L029F    tst   PD.UPC,y
          suba  #$20
 L02AE    rts
 
-L02AF    tst   PD.EKO,y       Echo turned on?
-         bne   L02BE
+CHKEKO    tst   PD.EKO,y       Echo turned on?
+         bne   EKOCHR
          cmpa  #$0D
          bne   L02BD
          tst   PD.ALF,y
-         bne   L02BE
+         bne   EKOCHR
 L02BD    rts
 
-L02BE    cmpa  #$20
-         bcc   L02C6
+EKOCHR    cmpa  #$20
+         bcc   EKOBYT
          cmpa  #$0D
-         bne   L02C9
-L02C6    lbra  L0396 Print character
+         bne   EKOCH2
+EKOBYT    lbra  PutDv2 Print character
 
 * Non-printable character
-L02C9    pshs  a
+EKOCH2    pshs  a
          lda   #'.
-         bsr   L02C6
+         bsr   EKOBYT
          puls  pc,a
-L02D1    ldx   D.Proc
+
+IODONE    ldx   D.Proc
          lda   ,x
          ldx   $03,y
          bsr   L02DB
@@ -398,25 +437,25 @@ L02DB    beq   L02E5
          clr   $04,x
 L02E5    rts
 
-L02E6    pshs  x,a
+GetDev    pshs  x,a
          ldx   $02,x
          lda   $04,x
-         beq   L0308
+         beq   GetDev10
          cmpa  ,s
          beq   L0324
          pshs  a
-         bsr   L02D1
+         bsr   IODONE
          puls  a
          os9   F$IOQu
          inc   $0F,y
          ldx   D.Proc
          ldb   P$Signal,X
          puls  x,a
-         beq   L02E6
+         beq   GetDev
          coma
          rts
 
-L0308    lda   ,s
+GetDev10    lda   ,s
          sta   $04,x
          sta   $03,x
          lda   <$2F,y
@@ -431,70 +470,79 @@ L0308    lda   ,s
 L0324    clra
          puls  pc,x,a
 
-L0327    ldx   D.Proc
+SCALOC    ldx   D.Proc
          lda   P$ID,x         Get process ID #
          clr   $0F,y
          ldx   $03,y
-         bsr   L02E6
+         bsr   GetDev
          bcs   L0341
          ldx   $0A,y
          beq   L033B
-         bsr   L02E6
+         bsr   GetDev
          bcs   L0341
 L033B    tst   $0F,y
-         bne   L0327
+         bne   SCALOC
          clr   $0C,y
 L0341    ldu   $06,y
          rts
 
-WriteLn  bsr   L0327
-         bra   L034C
+SCWrLine  bsr   SCALOC
+         bra   SCWrit00
 
-Write    bsr   L0327
+SCWrite    bsr   SCALOC
          inc   $0C,y
-L034C    ldx   $04,u
+SCWrit00    ldx   $04,u
          ldu   $06,u
-         beq   L038D
-L0352    lda   ,x+
+         beq   SCWrit99
+SCWrit10    lda   ,x+
          tst   $0C,y
          bne   L036C
-         lbsr  L029F
+         lbsr  UPCASE
          cmpa  #$0A
          bne   L036C
          lda   #$0D
          tst   <$25,y
          bne   L036C
-         bsr   L03A2
+         bsr   PutChr
          bcs   L0390
          lda   #$0A
-L036C    bsr   L03A2
+L036C    bsr   PutChr
          bcs   L0390
          leau  -$01,u
          cmpu  #$0000
-         bls   L038D
+         bls   SCWrit99
          lda   -$01,x
-         beq   L0352
+         beq   SCWrit10
          cmpa  <$2B,y
-         bne   L0352
+         bne   SCWrit10
          tst   $0C,y
-         bne   L0352
+         bne   SCWrit10
 L0385    ldu   $06,y
          tfr   x,d
          subd  $04,u
          std   $06,u
-L038D    lbra  L02D1
+SCWrit99    lbra  IODONE
+
 L0390    pshs  b,cc
          bsr   L0385
          puls  pc,b,cc
 
-* Print character
-L0396    pshs  u,x,a
+ page
+***************
+* PutChr
+*   put character in output buffer
+
+* Passed:  (A)=char
+*          (Y)=PD address
+* Destroys: B,CC
+PutDv2    pshs  u,x,a
          ldx   $0A,y
-         beq   L03F6
+         beq   PutChr9
          cmpa  #$0D
          bne   L03F8
          bra   L03D1
-L03A2    pshs  u,x,a
+
+PutChr    pshs  u,x,a
          ldx   $03,y
          cmpa  #$0D
          bne   L03F8
@@ -508,38 +556,40 @@ L03A2    pshs  u,x,a
          dec   $07,u
          bne   L03D1
          bra   L03C9
-L03BF    lbsr  L0276
+L03BF    lbsr  GetDv2
          bcs   L03C9
          cmpa  PD.PSC,y
          bne   L03BF
-L03C9    lbsr  L0276
+L03C9    lbsr  GetDv2
          cmpa  PD.PSC,y
          beq   L03C9
 L03D1    ldu   $02,x
          clr   $08,u
          lda   #$0D
-         bsr   L03FC
+         bsr   WrChar
          tst   $0C,y
-         bne   L03F6
-         ldb   PD.NUL,y
+         bne   PutChr9
+
+EOL         ldb   PD.NUL,y
          pshs  b
          tst   PD.ALF,y  Add linefeed?
          beq   L03ED
          lda   #$0A
-L03E9    bsr   L03FC
+EOL0    bsr   WrChar
          bcs   L03F4
 L03ED    lda   #$00
          dec   ,s
-         bpl   L03E9
+         bpl   EOL0
          clra
 L03F4    leas  1,s
-L03F6    puls  pc,u,x,a
+PutChr9    puls  pc,u,x,a
 
-L03F8    bsr   L03FC
+L03F8    bsr   WrChar
          puls  pc,u,x,a
 
-L03FC    ldu   #D$WRIT
-L03FF    pshs  u,y,x,a
+WrChar    ldu   #D$WRIT
+
+IOEXEC    pshs  u,y,x,a
          ldu   V$STAT,x
          clr   V.WAKE,u
          ldx   V$DRIV,x
